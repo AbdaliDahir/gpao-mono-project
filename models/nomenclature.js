fd6af8582d2347fb5924dd9162ee19ed831d203a
samurai <abdali.dahir@gmail.com>
2021-08-21 19:38:59 +0100

    nomenclature - remplacement schema

--- models/nomenclature.js
+++ models/nomenclature.js
@@ -1,37 +1,18 @@
 const mongoose = require('mongoose');
 
-// export enum Nomenclature_type {
-//   MP = 'MP',
-//   PF = 'PF',
-//   Piece = 'Piece',
-//   SE = 'SE'
-// }
-
-// export interface Nomenclature {
-//   reference: string;
-//   designation: string;
-//   type_fabrication_achat: string;
-//   unite_achat_stock: string;
-//   delai_en_semaine: number;
-//   prix_standard?: number;
-//   lot_de_reapprovisionnement?: number;
-//   stock_mini?: number;
-//   stock_maxi?: number;
-//   pourcentage_de_perte?: number;
-//   inventaire?: number;
-//   PF_ou_MP_ou_Piece_ou_SE: Nomenclature_type
-// }
-
 const nomenclatureSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+    compose: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
     },
-    icon: {
-        type: String,
+    composant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
     },
-    color: { 
-        type: String,
+    quantite_de_composition: {
+        type: Number
     }
 })
 
--- models/remplacement.js
+++ models/remplacement.js
@@ -1,52 +1,31 @@
 const mongoose = require('mongoose');
 
-// export enum Remplacement_type {
-//   MP = 'MP',
-//   PF = 'PF',
-//   Piece = 'Piece',
-//   SE = 'SE'
-// }
-
-// export interface Remplacement {
-//   reference: string;
-//   designation: string;
-//   type_fabrication_achat: string;
-//   unite_achat_stock: string;
-//   delai_en_semaine: number;
-//   prix_standard?: number;
-//   lot_de_reapprovisionnement?: number;
-//   stock_mini?: number;
-//   stock_maxi?: number;
-//   pourcentage_de_perte?: number;
-//   inventaire?: number;
-//   PF_ou_MP_ou_Piece_ou_SE: Remplacement_type
-// }
-
 const remplacementSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+    remplace_compose: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
     },
-    icon: {
-        type: String,
+    remplace_composant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
     },
-    color: { 
-        type: String,
+    remplacant_compose: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
+    },
+    remplacant_composant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        required:true
+    },
+    date_de_remplacement: {
+        type: Date,
+        default: Date.now,
     }
 })
-
-userSchema.methods.joiValidate = function(obj) {
-	var Joi = require('joi');
-	var schema = {
-		username: Joi.types.String().min(6).max(30).required(),
-		password: Joi.types.String().min(8).max(30).regex(/[a-zA-Z0-9]{3,30}/).required(),
-		email: Joi.types.String().email().required(),
-		first_name: Joi.types.String().required(),
-		last_name: Joi.types.String().required(),
-		created: Joi.types.Date(),
-	}
-	return Joi.validate(obj, schema);
-}
 
 remplacementSchema.virtual('id').get(function () {
     return this._id.toHexString();
